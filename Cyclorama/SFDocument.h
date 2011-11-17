ae0815c65d4d501ffb62d66d69862383053ac579
iain <iain@iains-imac.home>
2011-11-17 15:37:29 +0000

    Initial Commit

---
+++ Cyclorama/SFDocument.h
@@ -0,0 +1,13 @@
+//
+//  SFDocument.h
+//  Cyclorama
+//
+//  Created by Iain Holmes on 17/11/2011.
+//  Copyright (c) 2011 Sleep(5). All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SFDocument : NSDocument
+
+@end
