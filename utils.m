9042afd410a7e27d8cac23bf1ae18620cb1900a5
iain <iain@tonawanda.home>
2012-08-18 20:07:05 +0100

    Use the NSColor's CGColor message if running on Mountain Lion

--- utils.m
+++ utils.m
@@ -7,21 +7,28 @@
 //
 
 #import <Quartz/Quartz.h>
+#import "utils.h"
 
 CGColorRef CGColorCreateFromNSColor (NSColor *c)
 {
-    CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB();
     CGColorRef retColour;
-    NSColor *deviceColour = [c colorUsingColorSpaceName:NSDeviceRGBColorSpace];
-    CGFloat components[4];
     
-    [deviceColour getRed:&components[0] 
-                   green:&components[1] 
-                    blue:&components[2] 
-                   alpha:&components[3]];
+    // Awkward function no longer needed in Mountain Lion
+    if (NSAppKitVersionNumber <= NSAppKitVersionNumber10_7_2) {
+        CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB();
+        NSColor *deviceColour = [c colorUsingColorSpaceName:NSDeviceRGBColorSpace];
+        CGFloat components[4];
     
-    retColour = CGColorCreate(space, components);
-    CGColorSpaceRelease(space);
+        [deviceColour getRed:&components[0]
+                       green:&components[1]
+                        blue:&components[2]
+                       alpha:&components[3]];
+    
+        retColour = CGColorCreate(space, components);
+        CGColorSpaceRelease(space);
+    } else {
+        retColour = [c CGColor];
+    }
     
     return retColour;
 }
